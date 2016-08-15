1900e4e342f61f86b8b2d3451409633e967fc49d
ksibesh <sibesh.karn.92@gmail.com>
2016-08-15 19:28:26 +0530

    updates for redux changes

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,19 @@ var stats = require('./stats.generated.json');
 
 var app = express();
 app.use(express.static(path.join(__dirname, "..", "..", "public")));
+
+var createHistory = require('react-router/lib/createMemoryHistory');
+var createStore = require('redux').createStore;
+var syncHistoryWithStore = require('react-router-redux').syncHistoryWithStore;
+app.use(function(req, res) {
+  var memoryHistory = createHistory(req.orignalUrl);
+  console.log(memoryHistory);
+  // TODO: create reducers and pass that reducer as argument to function
+  var store = createStore(function() {});
+  // TODO: find a way to sync history with store
+  // var history = syncHistoryWithStore(memoryHistory, store);
+});
+
 app.get("/", function(req, res){
   res.end(page(req, stats.assetsByChunkName.main))
 });
